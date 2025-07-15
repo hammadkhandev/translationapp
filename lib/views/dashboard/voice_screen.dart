adfe9ac42ee78a37ad833758bcc42315ab49d98e
“hammadkhandev” <hammadkhandev@gmail.com>
2025-07-15 07:11:43 -0700

    translation voice screen

--- lib/views/dashboard/voice_screen.dart
+++ lib/views/dashboard/voice_screen.dart
@@ -1,174 +1,147 @@
+// lib/views/dashboard/voice_screen.dart
+
 import 'dart:math' as math;
-
 import 'package:flutter/material.dart';
-import 'package:norsk_tolk/views/dashboard/voice_input.dart';
 import 'package:norsk_tolk/views/dashboard/widgets/mic_box.dart';
-
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:flutter_tts/flutter_tts.dart';
+import '../../service/openai_service.dart';
+import '../../utils/app_constns.dart';
 import 'language_selector.dart';
 
 class VoiceScreen extends StatefulWidget {
   const VoiceScreen({super.key});
-
   @override
   State<VoiceScreen> createState() => _VoiceScreenState();
 }
 
-class _VoiceScreenState extends State<VoiceScreen> with TickerProviderStateMixin{
-  String sourceLanguage = 'English USA';
-  String targetLanguage = 'French';
-  // Mic 1 state
-  String displayText1 = 'Please tab Mic to Talk';
-  bool _isListening1 = false;
-  bool _isTranslating1 = false;
+class _VoiceScreenState extends State<VoiceScreen> with TickerProviderStateMixin {
+  // Languages
+  String sourceLang = 'en_US';
+  String targetLang = 'fr_FR';
 
-// Mic 2 state
-  String displayText2 = 'Please tab Mic to Talk';
-  bool _isListening2 = false;
-  bool _isTranslating2 = false;
+  // Services
+  late stt.SpeechToText _speech1, _speech2;
+  late FlutterTts _tts;
+  late OpenAIService _translator;
 
-// Animations
-  late AnimationController _pulseController1;
-  late Animation<double> _pulseAnimation1;
+  // Mic states
+  String text1 = 'Tap Mic to talk';
+  bool listening1 = false, translating1 = false;
+  late AnimationController ctl1; late Animation<double> anim1;
 
-  late AnimationController _pulseController2;
-  late Animation<double> _pulseAnimation2;
+  String text2 = 'Tap Mic to talk';
+  bool listening2 = false, translating2 = false;
+  late AnimationController ctl2; late Animation<double> anim2;
+
   @override
   void initState() {
     super.initState();
+    _speech1 = stt.SpeechToText();
+    _speech2 = stt.SpeechToText();
+    _tts = FlutterTts();
+    _translator = OpenAIService(AppConstns.gptToke);
 
-    _pulseController1 = AnimationController(
-      duration: const Duration(milliseconds: 1000),
-      vsync: this,
-    );
-    _pulseAnimation1 = Tween<double>(
-      begin: 1.0,
-      end: 1.2,
-    ).animate(CurvedAnimation(
-      parent: _pulseController1,
-      curve: Curves.easeInOut,
-    ));
+    _speech1.initialize();
+    _speech2.initialize();
 
-    _pulseController2 = AnimationController(
-      duration: const Duration(milliseconds: 1000),
-      vsync: this,
-    );
-    _pulseAnimation2 = Tween<double>(
-      begin: 1.0,
-      end: 1.2,
-    ).animate(CurvedAnimation(
-      parent: _pulseController2,
-      curve: Curves.easeInOut,
-    ));
-  }
-  void _startListening1() async {
-    setState(() {
-      _isListening1 = true;
-      displayText1 = 'Listening...';
-    });
+    ctl1 = AnimationController(vsync: this, duration: Duration(milliseconds:1000))
+      ..repeat(reverse: true);
+    anim1 = Tween(begin:1.0,end:1.2).animate(CurvedAnimation(parent: ctl1, curve: Curves.ease));
+    ctl1.stop();
 
-    _pulseController1.repeat(reverse: true);
-    await Future.delayed(Duration(seconds: 2));
-
-    setState(() {
-      _isListening1 = false;
-      _isTranslating1 = true;
-      displayText1 = 'Processing...';
-    });
-
-    _pulseController1.stop();
-    _pulseController1.reset();
-
-    await Future.delayed(Duration(seconds: 1));
-
-    setState(() {
-      _isTranslating1 = false;
-      displayText1 = 'Bonjour, comment allez-vous?';
-    });
+    ctl2 = AnimationController(vsync: this, duration: Duration(milliseconds:1000))
+      ..repeat(reverse: true);
+    anim2 = Tween(begin:1.0,end:1.2).animate(CurvedAnimation(parent: ctl2, curve: Curves.ease));
+    ctl2.stop();
   }
 
-  void _startListening2() async {
-    setState(() {
-      _isListening2 = true;
-      displayText2 = 'Listening...';
-    });
-
-    _pulseController2.repeat(reverse: true);
-    await Future.delayed(Duration(seconds: 2));
-
-    setState(() {
-      _isListening2 = false;
-      _isTranslating2 = true;
-      displayText2 = 'Processing...';
-    });
-
-    _pulseController2.stop();
-    _pulseController2.reset();
-
-    await Future.delayed(Duration(seconds: 1));
-
-    setState(() {
-      _isTranslating2 = false;
-      displayText2 = 'Hola, ¿cómo estás?';
-    });
-  }
-
-  void _clearText1() {
-    setState(() {
-      displayText1 = 'Please tab Mic to Talk';
-    });
-  }
-
-  void _clearText2() {
-    setState(() {
-      displayText2 = 'Please tab Mic to Talk';
-    });
-  }
   @override
   void dispose() {
-    _pulseController1.dispose();
-    _pulseController2.dispose();
+    ctl1.dispose(); ctl2.dispose();
+    _tts.stop();
     super.dispose();
   }
 
+  Future<void> handleMic({
+    required int mic,
+    required stt.SpeechToText speech,
+    required AnimationController ctl,
+    required Function(String) setText,
+    required Function(bool) setListening,
+    required Function(bool) setTranslating,
+    required String currentLang,
+    required String otherLang,
+  }) async {
+    setListening(true);
+    setText('Listening...');
+    ctl.repeat(reverse: true);
+    await speech.listen(localeId: currentLang, onResult: (res) {
+      if (res.finalResult) speech.stop();
+    });
+    await Future.doWhile(() => speech.isListening ? Future.delayed(Duration(milliseconds:100)) : Future.value(false));
+    ctl.stop();
+
+    setListening(false);
+    setTranslating(true);
+    setText('Translating...');
+
+    final spoken = speech.lastRecognizedWords;
+    final translation = await _translator.translate(currentLang.split('_')[0], otherLang.split('_')[0], spoken);
+
+    setTranslating(false);
+    setText(translation);
+
+    await _tts.setLanguage(otherLang);
+    await _tts.speak(translation);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(
-        child: Column(
-          children: [
-            Transform.rotate(
-              angle: -math.pi / 1.0,
-              child: MicTextBox(
-                hintText: 'Please tab Mic to Talk',
-                displayText: displayText1,
-                isListening: _isListening1,
-                isTranslating: _isTranslating1,
-                onClear: _clearText1,
-                onMicTap: _startListening1,
-                animation: _pulseAnimation1,
-              ),
+    return SafeArea(child: Column(
+      children:[
+        // Mic 1 (source → target)
+        Transform.rotate(
+          angle: -math.pi,
+          child: MicTextBox(
+            hintText: 'Tap Mic to talk',
+            displayText: text1,
+            isListening: listening1,
+            isTranslating: translating1,
+            onClear: () => setState(() => text1 = 'Tap Mic to talk'),
+            onMicTap: () => handleMic(
+              mic:1, speech:_speech1, ctl:ctl1,
+              setText:(v)=>setState(()=>text1=v),
+              setListening:(v)=>setState(()=>listening1=v),
+              setTranslating:(v)=>setState(()=>translating1=v),
+              currentLang: sourceLang, otherLang: targetLang,
             ),
-      
-            LanguageSelector(
-              sourceLanguage: sourceLanguage,
-              targetLanguage: targetLanguage,
-            ),
-      
-            MicTextBox(
-              hintText: 'Please tab Mic to Talk',
-              displayText: displayText2,
-              isListening: _isListening2,
-              isTranslating: _isTranslating2,
-              onClear: _clearText2,
-              onMicTap: _startListening2,
-              animation: _pulseAnimation2,
-            ),
-      
-      
-          ],
+            animation: anim1,
+          ),
         ),
-      ),
-    );
+
+        LanguageSelector(
+          sourceLanguage: sourceLang,
+          targetLanguage: targetLang,
+        ),
+
+        // Mic 2 (target → source)
+        MicTextBox(
+          hintText: 'Tap Mic to talk',
+          displayText: text2,
+          isListening: listening2,
+          isTranslating: translating2,
+          onClear: () => setState(() => text2 = 'Tap Mic to talk'),
+          onMicTap: () => handleMic(
+            mic:2, speech:_speech2, ctl:ctl2,
+            setText:(v)=>setState(()=>text2=v),
+            setListening:(v)=>setState(()=>listening2=v),
+            setTranslating:(v)=>setState(()=>translating2=v),
+            currentLang: targetLang, otherLang: sourceLang,
+          ),
+          animation: anim2,
+        ),
+      ],
+    ));
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import firebase_core
 import flutter_tts
 import google_sign_in_ios
 import shared_preferences_foundation
+import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -19,4 +20,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
